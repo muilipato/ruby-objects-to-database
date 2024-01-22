db3b8bd3eec886f2e08a78e26219c2c14f479310
Patrick <muilipato@gmail.com>
2024-01-22 23:18:45 +0300

    added find_by_name to query the database by using the name column of a table

--- lib/song.rb
+++ lib/song.rb
@@ -48,5 +48,16 @@ class Song
       end
   end
 
+  def self.find_by_name name
+    sql = <<-SQL
+    SELECT * FROM songs WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db row
+    end.first
+  end
+
+
 end
 
